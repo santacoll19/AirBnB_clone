5e4075e0f7adfbec906a149a67b6a1df4d5d95c6
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-30 16:57:41 -0400

    task6. changed syntax in city and added getenv

--- models/city.py
+++ models/city.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from models.place import Place
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'),
+                          nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete")
 
-    def __init__(self, *args, **kwargs):
-        """
-            Init for inherited
-        """
-        super().__init__(*args, **kwargs)
+    else:
+        state_id = ""
+        name = ""
