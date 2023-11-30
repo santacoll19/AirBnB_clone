f45f7357864571dd300354150b111309edff4673
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-30 11:13:13 -0400

    task2. fixed objject creation syntax.2.3

--- console.py
+++ console.py
@@ -114,7 +114,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class with given parameters."""
+        """ Create an object of any class with given parameter."""
         arg = args.split()
         if not arg:
             print("** class name missing **")
