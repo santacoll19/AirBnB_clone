8b413ff68c5e01762fe2239488e7c8bdf21480f9
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-30 16:53:42 -0400

    task6. changed syntax in init, save, delete and to_dict functions

--- models/base_model.py
+++ models/base_model.py
@@ -27,18 +27,18 @@ class BaseModel:
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
         else:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                setattr(self, key, value)
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        cp_dict = dict(self.__dict__)
-        if '_sa_instance_state' in cp_dict:
-            del cp_dict['_sa_instance_state']
-        return '[{}] ({}) {}'.format(cls, self.id, cp_dict)
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def __repr__(self):
         """
@@ -61,8 +61,10 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update(
+            {'__class__': (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary:
