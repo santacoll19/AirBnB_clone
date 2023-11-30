60ca382d3541160d1587f166af0b6cad51b8cdb1
Pepesaur96 <jjvazquez96@gmail.com>
2023-11-30 18:36:26 -0400

    task6. changed syntax for docreate and doall functions

--- console.py
+++ console.py
@@ -113,33 +113,34 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, arg):
+    def do_create(self, args):
         """Creates a new instance of BaseModel"""
-        args = arg.split()
-        if not args:
+        arg = args.split()
+        if not arg:
             print("** class name missing **")
-        if args[0] not in HBNBCommand.classes:
+        if arg[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-        else:
-            new_instance = HBNBCommand.classes[args[0]]()
-            for param in args[1:]:
-                key_value = param.split("=")
-                if len(key_value) == 2:
-                    key, value = key_value[0], key_value[1]
-                    if value[0] == '"' and value[-1] == '"':
-                        value = value[1:-
-                                      1].replace('_', ' ').replace('\"', '"')
-                    elif '.' in value:
-                        try:
-                            value = float(value)
-                        except ValueError:
-                            pass
-                    else:
-                        try:
-                            value = int(value)
-                        except ValueError:
-                            pass
-                    setattr(new_instance, key, value)
+        new_instance = HBNBCommand.classes[args[0]]()
+        for param in args[1:]:
+            key_value = param.split("=")
+            if len(key_value) != 2:
+                continue
+            if not hasattr(new_instance, key):
+                continue
+            key, value = key_value[0], key_value[1]
+            if value[0] == '"' and value[-1] == '"':
+                value = value[1:-1].replace('_', ' ').replace('\"', '"')
+            elif '.' in value:
+                try:
+                    value = float(value)
+                except ValueError:
+                    continue
+            else:
+                try:
+                    value = int(value)
+                except ValueError:
+                    continue
+                setattr(new_instance, key, value)
         new_instance.save()
         print(new_instance.id)
 
@@ -223,11 +224,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            cls = HBNBCommand.classes[args]
+            for v in storage.all(cls).values():
+                print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for v in storage.all().values():
                 print_list.append(str(v))
 
         print(print_list)
