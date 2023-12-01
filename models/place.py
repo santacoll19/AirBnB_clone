d14cb1f106ca8e6ea2473cf2148b03ed5cdb3ffc
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-01 16:01:53 -0400

    try to fix place

--- models/place.py
+++ models/place.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models.review import Review
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey(
+                          'amenities.id'), primary_key=True, nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -17,3 +27,13 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship(
+        "Amenity", secondary=place_amenity, viewonly=False)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def reviews(self):
+            """getter attribute reviews that returns the list of Review instances with place_id equals to the current Place.id"""
+            from models import storage
+            return [review for review in storage.all(Review).values() if review.place_id == self.id]
