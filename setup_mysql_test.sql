4d0667e0cfc3c143cc1dc952810acee91bdcc2d9
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-01 18:11:18 -0400

    Trying to fix setupmysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,10 @@
--- This script creates a MySQL server with the database hbnb_test_db.
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+
+CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
+
+GRANT
+SELECT
+    ON performance_schema.* TO 'hbnb_test' @'localhost';
