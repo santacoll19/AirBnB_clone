d46e8d3c2c62a3fcc8ead82cd85ea2e1f0e4df9a
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-01 18:15:20 -0400

    Trying to fix amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String, Table
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from models.place import Place, place_amenity
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       back_populates="amenities")
