e52d2af2f2aab86fcdf78d0d346af032b8d5a317
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-01 18:28:36 -0400

    Trying to fix state

--- models/state.py
+++ models/state.py
@@ -4,25 +4,25 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from os import getenv
+import models
 from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
+                              cascade="all, delete, delete-orphan")
     else:
-        @property
-        def cities(self):
-            """Returns the list of City instances
-            with state_id equals to the current State.id"""
-            from models import storage
-            all_cities = storage.all(City)
-            state_cities = []
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    state_cities.append(city)
-            return state_cities
+        class State(BaseModel):
+
+            @property
+            def cities(self):
+                """Returns the list of City instances
+                with state_id equals to the current State.id"""
+                from models import storage
+                all_cities = storage.all(City)
+                return [city for city in all_cities.values()
+                        if city.state_id == self.id]
