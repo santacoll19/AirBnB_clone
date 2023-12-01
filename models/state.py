41eac6c108a9e2a15dd820426d8a70e2e60ba549
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-01 02:07:45 -0400

    task6. added delete-orphan to relationship function

--- models/state.py
+++ models/state.py
@@ -10,10 +10,10 @@ from models.city import City
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
-                              cascade="all, delete")
+                              cascade="all, delete-orphan")
     else:
         @property
         def cities(self):
